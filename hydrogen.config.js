483facc9aef84ec2da1c22520b9274b688c5c5f1
Ion Taban <ion.taban@gmail.com>
2023-03-05 00:54:55 +0200

    try 2

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -4,13 +4,10 @@ export default defineConfig({
   shopify: {
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
-    storeDomain:
-      process.env?.PUBLIC_STORE_DOMAIN || 'sparks-4342.myshopify.com',
-    storefrontToken:
-      process.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      '5da351baa4820c47c48ec31ae30e3a29',
+    storeDomain: 'sparks-4342.myshopify.com',
+    storefrontToken: '5da351baa4820c47c48ec31ae30e3a29',
     storefrontApiVersion: '2023-01',
-    storefrontId: process.env?.PUBLIC_STOREFRONT_ID || 'sparks-4342',
+    storefrontId: 'sparks-4342',
   },
   session: CookieSessionStorage('__session', {
     path: '/',
